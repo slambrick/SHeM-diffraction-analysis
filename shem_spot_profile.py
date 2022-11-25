c59abad15be1de23eb6fbf18c8afe900fab8b16a
slambrick <sam.m@lambrick.org.uk>
2022-11-25 16:57:39 +0000

    added line plots

--- shem_spot_profile.py
+++ shem_spot_profile.py
@@ -4,8 +4,12 @@
 Created on Sat Oct 15 09:51:59 2022
 
 @author: Sam Lambrick
+@contributions: Ke Wang
 
 A module for importing, analysing, and plotting SHeM spot profile data.
+
+The 2D Gaussian function is based on this StackOverflow post:
+    https://stackoverflow.com/questions/21566379/fitting-a-2d-gaussian-function-using-scipy-optimize-curve-fit-valueerror-and-m
 """
 
 import numpy as np
@@ -16,7 +20,19 @@ from matplotlib import cm # Colour palettes
 import matplotlib.gridspec as gridspec
 import os
 from scipy import interpolate as interp
+import pandas as pd
 
+
+def speed_ratio(P_0, d_noz):
+    """This fit is from M.Bergi2018 doi.org/10.1016/j.ultramic.2019.112833
+    and is to data/simulation from Toennies & Winkelman doi.org/10.1063/1.434448."""
+    a = 0.43
+    b = 0.76
+    c = 0.84
+    d = 5.2
+    mu = 1.97
+    lS = a*np.log10(P_0*d_noz) + b + c/(1 + np.exp(-d*(np.log10(P_0*d_noz) - mu)))
+    return(10**(lS))
 
 def morse_V(z, x, y, params=(8.03, 1.35, 1.08, 0.102, 4)):
     """Calculates a corrugated Morse potential of the form used by Celli et 
@@ -26,7 +42,7 @@ def morse_V(z, x, y, params=(8.03, 1.35, 1.08, 0.102, 4)):
     v = D0*(np.exp(-2*alpha*(z - Q)) - 2*np.exp(-alpha1*z))
     return(v)
 
-def twoD_Gaussian(xy, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
+def twoD_Gaussian(xy, amplitude, xo, yo, sigma_x, sigma_y, theta, f, g, h, k, l, m):
     '''Two dimensional gaussian for fitting.'''
     
     x, y = xy
@@ -36,24 +52,16 @@ def twoD_Gaussian(xy, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
     a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
     b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
     c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
-    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) 
+    tot = amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) 
                             + c*((y-yo)**2)))
-    return g.ravel()
+    #f, g, h, k, l, m = offset
+    tot = tot + f + g*x + h*y + k*x*y + l*x**2 + m*y**2
+    return tot.ravel()
 
-
-def gaussian_plus_background(xy, amplitude, xo, yo, sigma_x, sigma_y, theta, offset, a0, a1, a2):
-    '''Two dimensional gaussian for fitting. Include a background somehow...'''
-    
+def background(xy, f, g, h, k, l, m):
     x, y = xy
-    # TODO: add a rotation to this for more general fitting 
-    xo = float(xo)
-    yo = float(yo)    
-    a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
-    b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
-    c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
-    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) 
-                            + c*((y-yo)**2))) + a0*x + a1*y + a2*x*y
-    return g.ravel()
+    tot = f + g*x + h*y + k*x*y + l*x**2 + m*y**2
+    return(tot)
 
 
 def theta_of_z(z):
@@ -158,7 +166,7 @@ class SpotProfile:
         r, c = data['I'].shape
         alphas = np.repeat(np.resize(alphas, [1, c]), r, axis=0)
         zs = np.repeat(data['zs'], c, axis=1)
-        alphas = np.repeat(np.resize(alphas, [1, c]), r, axis=0)
+        #alphas = np.repeat(np.resize(alphas, [1, c]), r, axis=0)
         sP = cls(z = zs, alpha_rotator = alphas, I = data['I'])
         
         # The example image the spot was defined from was at 300deg
@@ -206,7 +214,75 @@ class SpotProfile:
         sP.set_alpha_zero(0)
         sP.calc_dK()
         return(sP)
+    
+    def select_by_var(self, var, value):
+        dif = np.abs(value - getattr(self, var))
+        ind = np.where(dif == np.min(dif))
+        chosen = getattr(self, var)[ind][0]
+        if chosen != value:
+            print('Using {} = {}deg.'.format(var, chosen))
+        z = self.z[ind]
+        theta = self.theta[ind]
+        DK = self.DK[ind]
+        I = self.signal[ind]
+        alpha = self.alpha[ind]
+        df =  pd.DataFrame({'z' : z, 'theta' : theta, 'DK' : DK, 'signal' : I, 'alpha': alpha})
+        df.drop(var, axis=1, inplace=True)
+        return((df, chosen))
 
+    def select_alpha(self, alpha):
+        df, chosen_alpha = self.select_by_var('alpha', alpha)
+        return((df, chosen_alpha))
+    
+    def select_theta(self, theta):
+        df, chosen_theta = self.select_by_var('theta', theta)
+        return((df, chosen_theta))
+    
+    def select_z(self, z):
+        df, chosen_z = self.select_by_var('z', z)
+        return((df, chosen_z))
+    
+    def select_DK(self, DK):
+        df, chosen_DK = self.select_by_var('DK', DK)
+        return((df, chosen_DK))
+    
+    def line_plot(self, xvar, var, value, figsize=[5, 3.5], logplot=False):
+         df, chosen = self.select_by_var(var, value)
+         f = plt.figure(figsize=figsize)
+         ax = f.add_axes([0,0,1,1])
+         if logplot:
+             df['signal'] = np.log10(df['signal'])
+         df.plot(x = xvar, y = 'signal', ax = ax)
+         if logplot:
+             ax.set_ylabel('$\\log_{10}$(Intensity/nA)')
+         else:
+             ax.set_ylabel('Intensity/nA')
+         ax.grid(True)
+         return(f, ax, df, chosen)
+     
+    def line_plot_raw(self, alpha, figsize=[5, 3.5], logplot=False):
+        f, ax, df, chosen_alpha =self.line_plot('z', 'alpha', alpha, figsize=figsize, logplot=logplot)
+        ax.set_xlabel('$z/\\mathrm{mm}$')
+        ax.set_title('1D plot of a zscan at $\\alpha$={}$^\\circ$'.format(chosen_alpha))
+        return(f, ax, df)
+    
+    def line_plot_diffraction(self, alpha, figsize=[5, 3.5], logplot=False):
+        f, ax, df, chosen_alpha =self.line_plot('DK', 'alpha', alpha, figsize=figsize, logplot=logplot)
+        ax.set_xlabel('$\\Delta K/\\mathrm{nm}^{-1}$')
+        ax.set_title('1D diffraction scan at $\\alpha$={}$^\\circ$'.format(chosen_alpha))
+        return(f, ax, df)
+    
+    def line_plot_theta(self, alpha, figsize=[5, 3.5], logplot=False):
+        f, ax, df, chosen_alpha =self.line_plot('theta', 'alpha', alpha, figsize=figsize, logplot=logplot)
+        ax.set_xlabel('$\\theta_f/^\\circ$')
+        ax.set_title('1D diffraction scan at $\\alpha$={}$^\\circ$'.format(chosen_alpha))
+        return(f, ax, df)
+    
+    def line_plot_alpha(self, z, figsize=[5, 3.5], logplot=False):
+        f, ax, df, chosen_alpha =self.line_plot('alpha', 'z', z, figsize=figsize, logplot=logplot)
+        ax.set_xlabel('$\\alpha/^\\circ$')
+        ax.set_title('$\\alpha$ scan at z={}mm'.format(z))
+        return(f, ax, df)
     
     def calc_dK(self):
         '''Calculates the momentum transer for the data file and the
@@ -235,21 +311,34 @@ class SpotProfile:
         # TODO: tbh this simple version appears to be working fine, but I'm not
         # 100% convinced by it...
     
-    def shem_diffraction_plot(self, colourmap = cm.viridis):
+    def shem_diffraction_plot(self, colourmap = cm.viridis, bar_location='right', figsize=[8,6], rasterized=True):
         sP = self.filter_by_var('z', 2)
-        fig = plt.figure(figsize=[8,6])
-        gs = fig.add_gridspec(1,2,width_ratios=[10,0.5])
-        ax1=plt.subplot(gs[0], projection="polar", aspect=1.)
-        ax2 = plt.subplot(gs[1])
+        fig = plt.figure(figsize=figsize)
+        if bar_location == 'right':
+            gs = fig.add_gridspec(1,2,width_ratios=[10,0.5])
+            ax1=plt.subplot(gs[0], projection="polar", aspect=1.)
+            ax2 = plt.subplot(gs[1])
+        elif bar_location == 'bottom':
+            # The subplot size on the grid is determining the width of the colorbar
+            gs = fig.add_gridspec(2,3,height_ratios=[10,0.5], width_ratios=[1,4,1])
+            ax1=plt.subplot(gs[0:3], projection="polar", aspect=1.)
+            ax2 = plt.subplot(gs[4])
+        else:
+            raise ValueError('Unknon colorbar location.')
         mesh1 = ax1.pcolormesh(sP.alpha*np.pi/180, -sP.DK, np.log10(sP.signal), 
-                               edgecolors='face', cmap = colourmap)
+                               edgecolors='face', cmap = colourmap,  rasterized=rasterized)
         ax1.set_xlabel('$\\alpha$')
         ax1.grid()
         ax1.set_yticks([0, 25, 50, 75])
         ax1.tick_params(axis='y', colors=[0.9,0.9,0.9])
         #ax1.annotate('DK/nm^-1', (28*np.pi/180, 92), annotation_clip=False)
         add_scale(ax1, label = '$\Delta K/\\mathrm{nm}^{-1}$')
-        fig.colorbar(mesh1, cax=ax2, label="$\\log_{10}(I/\\mathrm{nA})$")
+        if bar_location == 'right':
+            fig.colorbar(mesh1, cax=ax2, label="$\\log_{10}(I/\\mathrm{nA})$")
+        elif bar_location == 'bottom':
+            ax2.set
+            fig.colorbar(mesh1, cax=ax2, label="$\\log_{10}(I/\\mathrm{nA})$", orientation='horizontal')
+            plt.subplots_adjust(hspace=0.3)
         return(fig, ax1, ax2)
     
     def shem_raw_plot(self, colourmap = cm.viridis):
@@ -285,10 +374,6 @@ class SpotProfile:
         sP.calc_dK()
         return(sP)
     
-    #def line_plot(self, var, alpha):
-        # TODO: extract the data for the specified alpha
-        # TODO: produce the line plot depending on the variable specified
-    
     def grid_interpolate(self, kx, ky, N=101, method='nearest'):
         '''Produces a 2D cartesian grid of the data for the provided kx and ky
         vectors.'''
@@ -299,12 +384,15 @@ class SpotProfile:
                         self.signal.flatten(), (kxx, kyy), method=method)
         return(I, kxx, kyy)
     
-    def interpolated_plot(self, kx=(-80, 80), ky=(-80,80), N=101, method='nearest'):
+    def interpolated_plot(self, kx=(-80, 80), ky=(-80,80), N=101, method='nearest', ax=None):
         '''Produce an interpolated plot of the data with N points along the
         kx and ky axes. Useful for checking the output of interpolation'''
         I, kxx, kyy = self.grid_interpolate(kx, ky, N, method=method)
-        f = plt.figure()
-        ax = f.add_axes([0.15, 0.1, 0.7, 0.9])
+        if ax == None:
+            f = plt.figure()
+            ax = f.add_axes([0.15, 0.1, 0.7, 0.9])
+        else:
+            f = ax.get_figure()
         ax.pcolormesh(kxx, kyy, np.log10(I), edgecolors='face')
         ax.axis('equal')
         ax.set_xlabel('$k_x/\mathrm{nm}^{-1}$')
